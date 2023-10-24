bdc135cb44db54100fdb2453d631a68fe14613e1
gon <valerianaman@gmail.com>
2023-10-24 15:10:46 +0200

    programatical constraints

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -17,6 +17,25 @@ class MapViewController: UIViewController{
         
         view = mapView
         
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+    
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:8)
+
+        
+        let margins = view.layoutMarginsGuide
+        
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
     }
     
 }
